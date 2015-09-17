73aa824c966edd4713f73ac5b7e6a91196bb7950
nicoeg <nicomanden@gmail.com>
2015-09-17 12:35:47 +0200

    Added Peters changest to server

--- AuctionHouse/AuctionHouse/ThreadMonitor.cs
+++ AuctionHouse/AuctionHouse/ThreadMonitor.cs
@@ -10,12 +10,17 @@ namespace AuctionHouse {
         public delegate void ThreadEventType(string message);
         public event ThreadEventType NewBidEvent;
         public event ThreadEventType ChangeBiddingItemEvent;
+        public event ThreadEventType UpdateTimeRemainingEvent;
 
         public void NewBid(string message) {
             NewBidEvent(message);
         }
         public void ChangeBiddingItem(string message) {
             ChangeBiddingItemEvent(message);
+        }
+
+        public void UpdateTimeRemaining(string message) {
+            UpdateTimeRemainingEvent(message);
         }
     }
 }
