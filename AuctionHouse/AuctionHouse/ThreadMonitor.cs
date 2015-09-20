35d99b1095dd25b4a2d27ae94eb93322a3b90213
Nicolaj Egelund <nicolaj@codepan.dk>
2015-09-20 19:43:51 +0200

    Removed unused projects and usings

--- AuctionHouse/AuctionHouse/ThreadMonitor.cs
+++ AuctionHouse/AuctionHouse/ThreadMonitor.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace AuctionHouse {
+﻿namespace AuctionHouse {
     class ThreadMonitor {
         public delegate void ThreadEventType(string message);
         public event ThreadEventType NewBidEvent;
--- AuctionHouse/PeterServer/MainWindow.xaml.cs
+++ AuctionHouse/PeterServer/MainWindow.xaml.cs
@@ -4,18 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace PeterServer {
